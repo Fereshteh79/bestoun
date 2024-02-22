7ba86e39535cace5cc9e00f1de9998c0c9d6dca7
USERNAME <EMAIL>
2024-02-22 03:55:10 +0330

    main changes

--- web/models.py
+++ web/models.py
@@ -1,31 +1,49 @@
-from django.contrib.auth.models import User
+from __future__ import unicode_literals
+from django.utils.crypto import get_random_string
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)  # TODO: do not save password
 
 
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255)
-    amount = models.BigIntegerField()
+    token = models.CharField(max_length=48)
 
     def __str__(self):
-        return "{}-{}".format(self.token, self.amount)
+        return "{}_token".format(self.user)
 
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCAD)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{}-{}-{}".format(self.text, self.amount, self.date)
+        return "{}-{}-{}-{}".format(self.text, self.date, self.user, self.amount)
 
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCAD)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{}-{}-{}".format(self.text, self.amount, self.date)
+        return "{}-{}-{}-{}".format(self.text, self.date, self.user, self.amount)
--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-
 from . import views
 
 urlspatterns = [
-    path('^submit/expense/$', views.submit_expense, name='submit_expense')
+    path(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
+    path(r'^edit/expense/?$', views.edit_expense, name='edit_expense'),
+    path(r'^submit/income/?$', views.submit_income, name='submit_income'),
+    path(r'^edit/income/?$', views.edit_income, name='edit_income'),
+    path(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
+    path(r'^q/incomes/?$', views.query_incomes, name='query_incomes'),
+    path(r'^q/expenses/?$', views.query_expenses, name='query_expenses'),
+    path(r'^accounts/register/?$', views.register, name='register'),
+    path(r'^accounts/whoami/?$', views.whoami, name='whoami'),
+    path(r'^accounts/login/?$', views.login, name='login'),
+    path(r'^news/?$', views.news, name='news'),
+    path(r'^$', views.index, name='index'),
 
 ]
